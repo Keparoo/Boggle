6b64c439b2030626137d32f7bb240ef0f1778e8e
Kep <KepCreations@gmail.com>
2021-10-05 18:43:45 -0400

    Set up basic Flask app

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+from flask import Flask, request, render_template, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "this-is-a-new-secret!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.debug = False # Turn off toolbar
+
+debug = DebugToolbarExtension(app)
+
+app.route('/')
+def index():
+    pass
