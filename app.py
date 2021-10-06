6943b6794c1b25daa28262c0f650ae32be134679
Kep <KepCreations@gmail.com>
2021-10-05 23:55:21 -0400

    Successful axios send

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 from flask import Flask, request, render_template, redirect, session, jsonify
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
-
-boggle_game = Boggle()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "this-is-a-new-secret!"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.debug = False # Turn off toolbar
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.debug = False # Turn off toolbar
+# debug = DebugToolbarExtension(app)
 
-debug = DebugToolbarExtension(app)
+boggle_game = Boggle()
 
-@app.route('/')
+@app.route("/")
 def index():
+    """Display game board"""
 
     board = boggle_game.make_board()
     session['board'] = board
 
     return render_template('index.html', board=board)
 
-@app.route('/check-word')
+@app.route("/check-word")
 def check_word():
+    """Check if submitted word is in dictionary"""
 
-    word = request.args('word')
-    print('word', word)
+    word = request.args["word"]
+    board = session["board"]
 
-    return jsonify({'result': 'Test'})
+    result = boggle_game.check_valid_word(board, word)
+    # result either 'ok' or 'not-word'
+
+    return jsonify({'result': result})
 
--- static/boggle.js
+++ static/boggle.js
@@ -1,15 +1,14 @@
 const $body = $('body');
 
 const handleGuess = async (e) => {
-	console.debug('handleGuess');
-
 	e.preventDefault();
+	console.debug('handleGuess');
 
 	const word = $('#guess').val();
 	console.log(word);
 
-	const response = axios.post('/check-word', { params: { word: word } });
+	const response = await axios.get('/check-word', { params: { word: word } });
 	console.log('response', response);
 };
 
-$body.on('submit', '#guessBtn', handleGuess);
+$body.on('click', '#guessBtn', handleGuess);
